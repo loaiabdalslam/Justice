cfcf4c7d5f4bd738bc599de82a2e9986bec5f25e
Mohamed2del <Mohamedadelmohamed997@gmail.com>
2019-08-30 03:26:29 +0200

    Add_room function without checking for duplicated rooms becuase its incermental id now untill we hash it

--- contracts/beta/Elections.sol
+++ contracts/beta/Elections.sol
@@ -4,75 +4,40 @@ contract VotingRoom {
 
     struct Voter
     {
-        uint PlayerId;
+        uint VoterId;
         address payable userAddr;
     }
 
 
     struct Room{
         uint[] VotersCount;
-        uint roomList;
+        uint roomId;
         mapping(uint => Voter) votersStructs;
         Voter player;
     }
 
     mapping(uint => Room) roomsStructs;
     uint[] allrooms;
-
+    
+    //  to prevent duplication
+    uint countRooms = 0;
 
     //Create New Voting Channel
 
-    function newRoom(uint roomnumber) public returns(bool success)
+    function newRoom() public returns(bool success)
     {
-        //To check Channel is created or not!
-        for(uint i =1; i <=roomnumber; i++){
-            require(roomsStructs[roomnumber].roomList != i );
-        }
-        roomsStructs[roomnumber].roomList = roomnumber;
-        allrooms.push(roomnumber);
+      
+        roomsStructs[countRooms].roomId = countRooms;
+        allrooms.push(countRooms);
+        
+        // just to keep it  simple untill we do hash functions 
+        countRooms ++ ;
+        
         return true;
     }
-
-
-    //Get Channel Details
-    function getRoom(uint roomnumber) public view returns(uint votersCounts)
+    
+    function test_str(uint id) public view returns (uint x)
     {
-        //To check Channel is created or not!
-        uint roomNum = roomsStructs[roomnumber].roomList;
-        if(roomNum != 0){
-            return(roomsStructs[roomnumber].VotersCount.length);
-        }
-        else{
-            revert("Room is not created!!");
-        }
+     return roomsStructs[id].roomId;   
     }
-
-
-
-
-    //To Add Voter in Channel
-    function addVoter(uint roomnumber,uint roomId,uint maxVoterCount) public returns(bool message)
-    {
-        //To check Channel is created or not!
-        uint roomNum = roomsStructs[roomnumber].roomList;
-        if(roomNum != 0){
-            uint voterCounter = roomsStructs[roomnumber].VotersCount.length;
-            if(voterCounter < maxVoterCount){
-                roomsStructs[roomnumber].VotersCount.push(roomId);
-                roomsStructs[roomnumber].votersStructs[roomId].userAddr = msg.sender;
-                roomsStructs[roomnumber].votersStructs[roomId].PlayerId = roomId;
-                return true;
-            }
-            else{
-                revert("Enter in new Channel, its full!!");
-            }
-        }
-        else{
-            revert("Channel is not created!!");
-        }
-    }
-
-
-
-
 }
